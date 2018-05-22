a81fcddb88f6c60a0b731192c0abc481f23bb30f
Vincent Morello <vmorello@gmail.com>
2018-05-22 19:37:21 +0100

    Experimenting with easier way to parse PRESTO .inf files

--- dynspec/presto_inf.py
+++ dynspec/presto_inf.py
@@ -2,34 +2,51 @@ import os
 import json
 
 
+
+# NOTE: Have a look at this, the original C code that reads/writes .inf files
+# https://github.com/scottransom/presto/blob/master/src/ioinf.c
+
+# The official way to parse is based on line order:
+# Phase 1: basename, telescope, instrument, source, raj, decj, observer, mjd, nsamp, tsamp, num_onoff (11 keys)
+# Parse on off pairs if any
+# Parse EM Band (1 key)
+# Depending on EM band, different things happen. We care about Radio.
+# Phase 2: fov, dm, fbot, bw, nchan, cbw, analyst (7 keys)
+
+# A character '=' is expected on column 40 of each line
+
+
+def lowercase_stripped(s):
+    """ Cast string to lowercase, strip spaces on the edges, and then replace
+    sequences of multiple spaces by a single one. This function is applied
+    to the value descriptions in a .inf file. """
+    return " ".join(s.lower().strip().split())
+
 class PrestoInf(dict):
     """ Parse PRESTO's .inf files that contain dedispersed time series
     metadata. """
 
-    parsing_guide = {
-        }
-
-    final_key_types = {
-        }
+    _KEYVAL_SEP = "= " # Note the trailing space
 
     def __init__(self, fname):
         self._fname = os.path.realpath(fname)
 
         with open(self.fname) as fobj:
-            lines = fobj.read().strip().split('\n')
+            lines = fobj.read().rstrip().split('\n')
 
-
+        self.lines = lines
 
         raw_attrs = {}
+        sep = self._KEYVAL_SEP
         for line in lines:
             try:
-                key, val = line.split('=')
-                key = key.strip().lower()
+                descr, val = line.split(sep)
+                descr = descr.strip()
                 val = val.strip()
-            except:
-                continue
-            key = key.strip().lower()
-            raw_attrs[key] = val
+            except Exception as err:
+                print(type(err))
+                print(err)
+            raw_attrs[descr] = val
 
         super(PrestoInf, self).__init__(raw_attrs)
 
@@ -44,5 +61,7 @@ class PrestoInf(dict):
         return None
 
 if __name__ == '__main__':
-    inf = PrestoInf("/home/vince/work/pulsars/presto_time_series/J1855+0307/J1855+0307_DM400.00.inf")
-    print(json.dumps(inf, indent=4))
+    #inf = PrestoInf("/home/vince/work/pulsars/presto_time_series/J1855+0307/J1855+0307_DM400.00.inf")
+    inf = PrestoInf("LOTAAS.inf")
+    for key in sorted(inf.keys()):
+        print('\"' + key + '\"', ':', inf[key])
